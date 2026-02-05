74a4568712b5be5025bbb524aec41f33496a39e7
tschoi-macbook-air <eldis2000@gmail.com>
2026-02-05 10:32:50 +0900

    rag 어드민 연결

--- nginx/html/js/main.js
+++ nginx/html/js/main.js
@@ -156,6 +156,10 @@ function setupEventListeners() {
 
     $("#menuToggle, #closeSidebar").on("click", toggleSidebar);
 
+    $("#settings-button").on("click", function() {
+        window.open("http://43.201.28.177:8000", "rag_admin");
+    });
+
     $(".department-item").on("click", function () {
         const dept = $(this).data("department");
         selectDepartment(dept);
